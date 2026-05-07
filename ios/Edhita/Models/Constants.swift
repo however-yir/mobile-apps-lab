7b1c0e200e8dc413f252e51863e0aa7a74a96361
however <however-yir@users.noreply.github.com>
2026-05-07 22:08:25 +0800

    fix: iOS CI missing Constants.swift + link-check --exclude-mail
    - Remove Constants.swift from ios/.gitignore so it gets tracked in git
    - Link-check workflow already had --exclude-mail removed in prior edit
    - Fixes iOS CI build failure and Link Check failure

---
+++ ios/Edhita/Models/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Constants {
+    static let adUnitID = "ca-app-pub-xxxxxxxxxxxxxxxx/nnnnnnnnnn"
+    #if DEBUG
+        static let enableAd = false
+        static let debugGDPR = false
+    #endif
+}
